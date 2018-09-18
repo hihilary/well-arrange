392ae8afada1d51317d58fa441998da878ffde6e
HilaryDeng <hihilary@outlook.com>
2018-09-18 21:55:08 +0100

    complete the function

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import FileDropZone from './FileDropZone';
 import {connect} from 'react-redux'
 import {IStoreState} from './interface'
 
+import * as XLSX from 'xlsx'
+
 interface IProps{
   file1: any,
   file2: any,
@@ -35,11 +37,34 @@ class App extends React.Component<IProps, any> {
     const file3 = this.props.file3
     const file4 = this.props.file4
     if (file1!=='' && file2!=='' && file3 !== '' && file4 !== '') {
-        console.log(file1)
+      const ret = this.arrange(file1, file2, file3, file4)
+      console.log(ret)
+      const worksheet = XLSX.utils.aoa_to_sheet(ret);
+      const newWorkbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(newWorkbook, worksheet, "sheet1");
+      XLSX.writeFile(newWorkbook, 'new.xlsx');
+    } else {
+      alert('not enough files')
+    }
+  }
 
-      } else {
-        alert('not enough files')
+  private arrange = (file1:[], file2:[], file3:[], file4:[]) :any[] => {
+    // console.log(file1, file2, file3, file4)
+    const arranged:any[] = []
+    arranged.push(file1[0])
+    for (let i = 0; i < 8; ++i) {
+      // line of odd number
+      for (let j = 1; j <= 12; ++j) {
+        arranged.push(file1[12*i + j])
+        arranged.push(file2[12*i + j])
       }
+      // line of even number
+      for (let j = 1; j <= 12; ++j) {
+        arranged.push(file3[12*i + j])
+        arranged.push(file4[12*i + j])
+      }
+    }
+    return arranged
   }
 }
 
